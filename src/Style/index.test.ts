0b459bd48f2d99c34a3b96b490ab28ca411c276a
Jesse Kelly <kellyjesse881@gmail.com>
2021-09-21 15:27:33 -0500

    feat: expended toCss to map margin

--- src/Style/index.test.ts
+++ src/Style/index.test.ts
@@ -12,11 +12,28 @@ describe("Style", () => {
                 bottom: em(1),
             }
 
-            expect(toCss({ padding })).toEqual({
+            expect(toCss({ padding } as Style)).toEqual({
                 paddingRight: '1rem',
                 paddingLeft: '1px',
                 paddingTop: '1vh',
                 paddingBottom: '1em'
+            })
+        })
+
+
+        test("margin", () => {
+            const margin: Style["margin"] = {
+                right: rem(1),
+                left: px(1),
+                top: vh(1),
+                bottom: em(1),
+            }
+
+            expect(toCss({ margin } as Style)).toEqual({
+                marginRight: '1rem',
+                marginLeft: '1px',
+                marginTop: '1vh',
+                marginBottom: '1em'
             })
         })
     })
--- src/Style/index.ts
+++ src/Style/index.ts
@@ -3,6 +3,7 @@ import { pipe } from 'fp-ts/function';
 
 import { LengthPercent, showLengthPercent } from '../Length';
 
+
 type PartialRecord<K extends keyof any, T> = {
   [P in K]?: T;
 };
@@ -13,12 +14,19 @@ export const vertical: Edge[] = ["bottom", "top"];
 
 // Internal representation.
 export interface Style {
-    padding: PartialRecord<Edge, LengthPercent>
+    padding: PartialRecord<Edge, LengthPercent>,
+    margin: PartialRecord<Edge, LengthPercent>,
 }
+
 
 export const toCss = (style: Style): CSS.Properties => ({
     paddingRight: style?.padding?.right && pipe(style.padding.right, showLengthPercent.show),
     paddingLeft: style?.padding?.left && pipe(style.padding.left, showLengthPercent.show),
     paddingTop: style?.padding?.top && pipe(style.padding.top, showLengthPercent.show),
     paddingBottom: style?.padding?.bottom && pipe(style.padding.bottom, showLengthPercent.show),
+
+    marginRight: style?.margin?.right && pipe(style.margin.right, showLengthPercent.show),
+    marginLeft: style?.margin?.left && pipe(style.margin.left, showLengthPercent.show),
+    marginTop: style?.margin?.top && pipe(style.margin.top, showLengthPercent.show),
+    marginBottom: style?.margin?.bottom && pipe(style.margin.bottom, showLengthPercent.show),
 })
