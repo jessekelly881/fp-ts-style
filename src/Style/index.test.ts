2a10c5317896ce6e9494a6c02ac21ae140e17a31
Jesse Kelly <kellyjesse881@gmail.com>
2021-09-21 16:14:31 -0500

    feat: added margin

--- src/Style/index.test.ts
+++ src/Style/index.test.ts
@@ -5,7 +5,7 @@ import { percent } from '../Percent';
 
 describe("Style", () => {
     describe("toCss", () => {
-        const { toCss, padding } = createStyle();
+        const { toCss, padding, margin } = createStyle();
 
         test("padding", () => {
             const top = pipe(padding("top")(px(1)), toCss);
@@ -17,6 +17,18 @@ describe("Style", () => {
             expect(bottom).toEqual({ paddingBottom: '5rem' })
             expect(right).toEqual({ paddingRight: '2vh' })
             expect(left).toEqual({ paddingLeft: '10%' })
+        })
+
+        test("margin", () => {
+            const top = pipe(margin("top")(px(1)), toCss);
+            const bottom = pipe(margin("bottom")(rem(5)), toCss);
+            const right = pipe(margin("right")(vh(2)), toCss);
+            const left = pipe(margin("left")(percent(10)), toCss);
+
+            expect(top).toEqual({ marginTop: '1px' })
+            expect(bottom).toEqual({ marginBottom: '5rem' })
+            expect(right).toEqual({ marginRight: '2vh' })
+            expect(left).toEqual({ marginLeft: '10%' })
         })
     })
 })
--- src/Style/index.ts
+++ src/Style/index.ts
@@ -18,25 +18,34 @@ export type StyleMap1<A> = (a: A) => StyleMap;
 export type StyleMap2<A, B> = (a: A) => (b: B) => StyleMap;
 export type StyleMap3<A, B, C> = (a: A) => (b: B) => (c: C) => StyleMap;
 
-
+type PropName = keyof CSS.Properties;
 
 // Internal representation.
 export interface StyleMaps {
     toCss: (map: StyleMap) => CSS.Properties,
     padding: StyleMap2<Edge, LengthPercent>,
+    margin: StyleMap2<Edge, LengthPercent>,
 }
 
-const paddingMap: Record<Edge, keyof CSS.Properties> = {
+const paddingMap: Record<Edge, PropName> = {
   "bottom": "paddingBottom",
   "top": "paddingTop",
   "left": "paddingLeft",
   "right": "paddingRight"
 }
 
+const marginMap: Record<Edge, PropName> = {
+  "bottom": "marginBottom",
+  "top": "marginTop",
+  "left": "marginLeft",
+  "right": "marginRight"
+}
+
 
 const createStyle = (): StyleMaps => ({
   toCss: map => map({}),
-  padding: edge => len => css => ({ ...css, [paddingMap[edge]]: showLengthPercent.show(len) })
+  padding: edge => len => css => ({ ...css, [paddingMap[edge]]: showLengthPercent.show(len) }),
+  margin: edge => len => css => ({ ...css, [marginMap[edge]]: showLengthPercent.show(len) })
 })
 
 export default createStyle;
