01bae40b92896019886d442977af5693a545e504
Jesse Kelly <kellyjesse881@gmail.com>
2021-12-29 14:31:41 -0600

    feat: converted ratio to prism

--- src/Ratio/index.test.ts
+++ src/Ratio/index.test.ts
@@ -1,9 +1,22 @@
 import * as R from '.';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
 
 
 describe("Ratio", () => {
     test("ratio", () => {
-        expect(R.Show.show(R.ratio(1))).toEqual("1 / 1");
-        expect(R.Show.show(R.ratio(1, 2))).toEqual("1 / 2");
+        expect(R.ratio(-1)).toEqual(O.none);
+        expect(R.ratio(0)).toEqual(O.none);
+        expect(R.ratio(1)).toEqual(O.some([1,1]));
+        expect(R.ratio(1, 2)).toEqual(O.some([1,2]));
+    })
+
+    test("Show", () => {
+        const str = pipe(
+            R.ratio(1,2),
+            O.fold(() => "", R.Show.show)
+        );
+
+        expect(str).toEqual("1 / 2");
     })
 })
--- src/Ratio/index.ts
+++ src/Ratio/index.ts
@@ -1,16 +1,21 @@
+import { Newtype, prism } from 'newtype-ts'
 import * as S from 'fp-ts/Show';
+import * as O from 'fp-ts/Option';
 
-type Ratio = [number, number] // Both numbers should be positive
+interface Ratio extends Newtype<{ readonly Ratio: unique symbol }, [number, number]> {}
+const isRatio = (a: [number, number]) => a[0] > 0 && a[1] > 0;
 
-export default Ratio;
+const prismRatio = prism<Ratio>(isRatio)
 
 /*
  * @example ratio(2) // [2, 1]  aka 2 / 1;
  * @example ratio(1, 2) // [1, 2]  aka 1 / 2;
  */
-export const ratio = (...arg: [number] | [number, number]): [number, number] =>
-    arg.length === 1 ? [arg[0], arg[0]] : arg;
+export const ratio = (...arg: [number] | [number, number]): O.Option<Ratio> =>
+    prismRatio.getOption(arg.length === 1 ? [arg[0], arg[0]] : arg)
 
 export const Show: S.Show<Ratio> = ({
-    show: (r: Ratio) => `${r[0]} / ${r[1]}`
+    show: (r: Ratio) => `${prismRatio.reverseGet(r)[0]} / ${prismRatio.reverseGet(r)[1]}`
 })
+
+export default Ratio;
