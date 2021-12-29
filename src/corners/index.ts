d97d63b4ecbcb47962e55035450f8dfafcdafdc8
Jesse Kelly <kellyjesse881@gmail.com>
2021-12-29 13:35:57 -0600

    change: changed corners

--- src/corners/index.ts
+++ src/corners/index.ts
@@ -1,8 +1,8 @@
-type Corders<T> = Partial<{
+type Corners<T> = Partial<{
     topLeft: T;
     topRight: T;
     bottomLeft: T;
     bottomRight: T;
 }>
 
-type Corner = "topLeft" | "topRight" | "bottomLeft" | "bottomRight" | "top" | "bottom" | "left" | "right" | "all";
+type Corner = keyof Corners<unknown>
